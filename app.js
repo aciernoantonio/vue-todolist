cd9cf8546372d7afb42cf594bf5b4180ff69948e
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-22 12:57:08 +0200

    add milestone 1

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ const app = new Vue({
 
     data: {
 
+        completed: `completed`,
+
+        tasks: [
+            {
+                text: `Learn HTML`,
+                done: true
+            },
+
+            {
+                text: `Learn Css`,
+                done: true
+            },
+
+            {
+                text: `Learn JavaScript (I hate him)`,
+                done: false
+            },
+
+            {
+                text: `Learn VueJs`,
+                done: false
+            }
+
+        ]
+
     },
 
     methods: {
-        
+
     }
 })
